babbc989a2d35d601f88a770b5081e949f326d9a
Milind Contractor <ai.dotproduct@gmail.com>
2021-11-20 14:17:03 +0800

    Initial Commit

---
+++ products/views.py
@@ -0,0 +1,14 @@
+from rest_framework import viewsets
+
+class ProductViewSet(viewsets.ViewSet):
+    def list(self, request):
+        pass
+    
+    def create(self, request, pk=None):
+        pass
+
+    def retrieve(self, request, pk=None):
+        pass 
+
+    def delete(self, request, pk=None):
+        pass

b983b6d5365b9132ca9441a8859ce9873de83b2d
Milind Contractor <ai.dotproduct@gmail.com>
2021-11-21 01:16:53 +0800

    Finished API

--- products/serializers.py
+++ products/serializers.py
@@ -1,8 +1,9 @@
-from rest_framework import Serializers
+from rest_framework import serializers
+
 from .models import Product
 
 
-class ProductSerializers(serializers.ModelSerializer):
+class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = "__all__"
+        fields = '__all__'
--- products/urls.py
+++ products/urls.py
@@ -14,9 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
 
+from .views import ProductViewSet, UserAPIView
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('products', ProductViewSet.as_view({
+        'get': 'list',
+        'post': 'create'
+    })),
+    path('products/<str:pk>', ProductViewSet.as_view({
+        'get': 'retrieve',
+        'put': 'update',
+        'delete': 'destroy'
+    })),
+    path('user', UserAPIView.as_view())
 ]
